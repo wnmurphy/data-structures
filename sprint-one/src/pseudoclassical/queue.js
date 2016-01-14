7cf77dad3aaa7f65f56e0c94bdfaa5ca459dd0f4
Neil Murphy <wnmurphy@gmail.com>
2016-01-14 11:19:10 -0800

    Passing tests for pseudoclassical stack.js and queue.js.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,32 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function(){
+  this.storage = {};
+  this.count = 0;
 };
 
 
+Queue.prototype.enqueue = function(value){
+  this.count++;
+  this.storage[this.count] = value;
+};
+
+Queue.prototype.dequeue = function(){
+
+    var dequeued = this.storage[1];
+    delete this.storage[1]; 
+
+    for(var i = 0; i < this.count; i++){ 
+      this.storage[i] = this.storage[i+1];
+    }
+    
+    if(this.count){ 
+      this.count--;
+    }
+
+    return dequeued;
+};
+
+Queue.prototype.size = function(){
+  return this.count;
+};
+
+var queueInstance = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
+Stack.prototype.push = function(value){
+  this.count++;
+  this.storage[this.count] = value;
+};
 
+Stack.prototype.pop = function(){
+  var popped = this.storage[this.count];
+  if(this.count){
+    this.count--;
+  }
+  return popped;
+};
+
+Stack.prototype.size = function(){
+  return this.count;
+};
+
+var stackInstance = new Stack();
