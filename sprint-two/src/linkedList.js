eb74ee033ecd6ef9c84cb775ecddccbe35af58f9
Neil Murphy <wnmurphy@gmail.com>
2016-01-14 20:09:09 -0800

    Passing tests for linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,18 +4,61 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
-  };
+
+    // declare a new Node(value)
+    var newNode = Node(value);
+
+    // if list is empty
+    if(this.head === null){ 
+
+      // set list's head to current node
+      this.head = newNode;
+
+      // store current node at tail
+      this.tail = this.head;
+
+    } else {
+      // {value: 4, next: null}
+      this.head.next = newNode;
+      // {value: 4, next: {value: 5, next: null}}
+      this.tail = newNode;
+      // {value: 4, next: null}
+    }
+  }; 
 
   list.removeHead = function(){
+    // store value of head to return
+    var headValue = this.head.value;
+
+    //reassign head
+    this.head = this.head.next;
+
+    return headValue;
   };
 
   list.contains = function(target){
+    // declare a variable to keep track of current node
+    var current = this.head;
+    var result = false; 
+  
+    while(current.next !== null){
+    if(current.value === target){
+        result = true;
+      }
+      current = current.next;
+    }
+    if(this.tail.value === target){
+      result = true;
+    }
+
+
+    return result;
   };
 
   return list;
 };
 
-var Node = function(value){
+function Node (value){
   var node = {};
 
   node.value = value;
