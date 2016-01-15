b7191f742a92cbf9a422327935e67d5481a24687
Neil Murphy <wnmurphy@gmail.com>
2016-01-15 15:29:24 -0800

    Passing tests for set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set.storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+   if(!this.storage[item]){
+     this.storage[item] = item;
+   } 
 };
 
 setPrototype.contains = function(item){
+  for(var key in this.storage){
+    if(key === item){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  for(var key in this.storage){
+    if(key === item){
+      delete this.storage[key];
+    }
+  }
 };
 
 /*
