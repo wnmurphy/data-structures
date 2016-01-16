09a7c9d15d53e76330cef254fa04e35a75f10e04
Neil Murphy <wnmurphy@gmail.com>
2016-01-15 20:35:33 -0800

    Passing tests for hashTable.js.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,3 +1,10 @@
+/* Helper functions:
+getIndexBelowMaxForKey // This is a "hashing function". Convert str to integer between 0 and max (str, max)
+limitedArray.get // return value at index (index)
+limitedArray.set // store value at index (value, index)
+limitedArray.each //run callback on each element (callback)
+*/
+
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -5,15 +12,36 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if(this._storage[i] === undefined){
+    this._storage[i] = [];
+  }
+  this._storage[i].push([k,v]);
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var result, i = getIndexBelowMaxForKey(k, this._limit);
+  for(var j = 0; j < this._storage[i].length; j++){
+    if(this._storage[i][j][0] === k){
+      result = this._storage[i][j][1];
+    }
+  }
+  if(result === undefined){
+    return null;
+  }else{
+    return result;
+  }
 };
 
 HashTable.prototype.remove = function(k){
+  var result, i = getIndexBelowMaxForKey(k, this._limit);
+  //this._storage[i] = null;
 
+  for(var j = 0; j < this._storage[i].length; j++){
+    if(this._storage[i][j][0] === k){
+      var spliced = this._storage[i].splice(j, 1);
+      this._storage[i]
+    }
+  }
 };
 
 
