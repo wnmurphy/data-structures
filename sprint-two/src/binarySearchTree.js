3e2f21ec0ba4acee31b13eaa1681552e3f511746
Neil Murphy <wnmurphy@gmail.com>
2016-01-16 09:41:18 -0800

    Passing test to .insert() values

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,45 @@
 var BinarySearchTree = function(value){
+  var binarySearchTree = Object.create(BinarySearchTree.prototype);
+  binarySearchTree.value = value;
+  binarySearchTree.left = null;
+  binarySearchTree.right = null;
+  return binarySearchTree;
+};
+
+//var node = new BinarySearchTree();
+
+BinarySearchTree.prototype.insert = function(value){
+
+  // if value is greater, go right
+  if (value > this.value){
+    if(this.right === null){
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }  
+  // if value is lesser, go left
+  } else if (value < this.value){
+    if(this.left === null){
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }  
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value){
+  // .contains(value)
+  // recursively search each node's value
+  // compare node's value to target value
+  // return boolean
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+  // basically swipe .contains and modify to be like .each()
 };
 
 
+//var binarySearchTree = new BinarySearchTree();
 /*
  * Complexity: What is the time complexity of the above functions?
  */
